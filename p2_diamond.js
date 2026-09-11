04aef4e8b88d97f842139c304f7530b08bbfc244
Yire Kim <rladlfp22@naver.com>
2026-09-11 13:35:35 +0900

    p2: diamond

--- p2_diamond.js
+++ p2_diamond.js
@@ -11,6 +11,26 @@
 
 const n = Number(process.argv[2] ?? 5);
 
+if (n % 2 === 0) {
+    console.log("Number is not odd.");
+    process.exit(1);
+}
+
+let spaces = Math.floor(n / 2);
+let stars = 1;
+
+for (let i = 0; i < n; i++) {
+    console.log(" ".repeat(spaces) + "*".repeat(stars) + " ".repeat(spaces));
+
+    if (i < Math.floor(n / 2)) {
+        spaces--;
+        stars += 2;
+    } else {
+        spaces++;
+        stars -= 2;
+    }
+}
+
 // TODO: print the diamond with for / if and console.log.
 // Hint: for row i, count the spaces and the stars separately.
 // Hint: "*".repeat(3) gives "***".
